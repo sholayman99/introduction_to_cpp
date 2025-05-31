5c6c96af3770f9222139596b810dda3b83835040
sholayman99 <mdsholayman998@gmail.com>
2025-05-31 19:29:39 +0600

    completed mid term

--- mid-term/Who_Is_It.cpp
+++ mid-term/Who_Is_It.cpp
@@ -8,9 +8,25 @@ int main()
 
     while (t--)
     {
-       int id;
-       int name;
-       char
+       int best_id = -1 , best_mark= -1;
+       char best_name[101],best_section;
+       
+       for(int i=0 ; i<3 ; i++)
+       {
+         int id,mark;
+         char name[101],section;
+
+         cin >> id >>  name >> section >> mark ;
+
+         if(i== 0 || mark > best_mark || (mark == best_mark && id < best_id))
+         {
+            best_id = id;
+            best_mark = mark;
+            best_section = section ;
+            strcpy(best_name,name);
+         }
+       }
+       cout << best_id << " " << best_name << " " << best_section << " " << best_mark << endl;
     }
     
 
